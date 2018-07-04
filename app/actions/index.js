59e9fbf148b52fac148aedfb86c52793c1c8699d
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-04 17:45:04 +0300

    Add action and reducer methods for the best top 20 movies fetching

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,5 +1,6 @@
 export const GET_MOVIES_BY_TITLE = 'GET_MOVIES_BY_TITLE';
 export const GET_LATEST_MOVIES = 'GET_LATEST_MOVIES';
+export const GET_TOP_20_MOVIES = 'GET_TOP_20_MOVIES';
 
 export const fetchMoviesByTitle = (query) => (dispatch) => {
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=17dadb414883c56a000864669faba791&query=${query}`, {
@@ -28,3 +29,18 @@ export const fetchLatestMovies = () => (dispatch) => {
             });
     }).catch((error) => console.log(error));
 }
+
+export const getTopRatedMovies = () => (dispatch) => {
+    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=17dadb414883c56a000864669faba791&page=1', {
+        method: 'GET'
+    }).then(response => {
+        response.json()
+            .then(data => {
+                dispatch({
+                    type: GET_TOP_20_MOVIES,
+                    payload: data.results
+                });
+            });
+    }).catch((error) => console.log(error));
+}
+
