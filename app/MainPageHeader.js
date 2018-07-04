8f854d22a74c8af3bf551357fec9a5ff8e0bb5f8
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-04 17:44:45 +0300

    Add LInk to BestMoviesPage in navigation and fix styles for it

--- app/MainPageHeader.js
+++ app/MainPageHeader.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { AppBar, Toolbar } from "@material-ui/core";
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 export default class MainPageHeader extends React.Component {
     render(){
         return <div>
                     <AppBar position="static">
                         <Toolbar>
-                            <a href="/">Home</a>
+                            <Link to="/">Home</Link>
+                            <Link className="bestRatedLink" to="bestMovies">Best Rated TOP 20</Link>
                             <a className="navRightLinks" href="https://bitbucket.org/minsma/">Made By M.S.</a>
                             <a href="https://www.themoviedb.org/">API: The Movie Database</a>
                         </Toolbar>
